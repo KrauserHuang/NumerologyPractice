e275b3d15926a3cbb3ef9ed9c42ed54fa8f029fe
krauserhuang <crescentdence@gmail.com>
2020-08-26 22:06:00 +0800

    Initial Commit

---
+++ NumerologyPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumerologyPractice
+//
+//  Created by Tai Chin Huang on 2020/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
