7fc31ef7a11de7573ee50a68bc5fce5b235dc702
krauserhuang <crescentdence@gmail.com>
2020-09-21 10:42:11 +0800

    2020/9/21 10:41 first edition!(problem not fix yet)

---
+++ NumerologyPractice/Detail.swift
@@ -0,0 +1,13 @@
+//
+//  Detail.swift
+//  NumerologyPractice
+//
+//  Created by Tai Chin Huang on 2020/9/5.
+//
+
+import Foundation
+
+struct numberDetail {
+    let number: String
+    let intro: String
+}
--- NumerologyPractice/ViewController.swift
+++ NumerologyPractice/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var birthPicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    //把資料從第一頁傳到第二頁
+    @IBSegueAction func revealTarot(_ coder: NSCoder) -> TarotViewController? {
+        //透過coder建立從storyboard設計的controller，包含了建立controller畫面需要的內容
+        let controller = TarotViewController(coder: coder)
+        //將第一頁的birthPicker.date存到第二頁的birthday
+        controller?.birthday = birthPicker.date
+        //回傳給TarotViewController
+        return controller
+    }
+    
 
 }
 
